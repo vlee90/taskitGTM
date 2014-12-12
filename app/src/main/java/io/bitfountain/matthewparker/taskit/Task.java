ac0679004ffa16cf0158492bb128dcfbf5abbd5c
Matt Parker <mdparker89@gmail.com>
2014-12-12 12:24:49 -0500

    create task class

---
+++ app/src/main/java/io/bitfountain/matthewparker/taskit/Task.java
@@ -0,0 +1,12 @@
+package io.bitfountain.matthewparker.taskit;
+
+import java.util.Date;
+
+/**
+ * Created by matthewparker on 12/12/14.
+ */
+public class Task {
+    private String mName;
+    private Date mDueDate;
+    private boolean mDone;
+}
