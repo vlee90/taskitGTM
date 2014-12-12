2f299e9c0515461759fa17ea6ecdb2c541e0ccc2
Matt Parker <mdparker89@gmail.com>
2014-12-12 12:34:59 -0500

    add getters and setters

--- app/src/main/java/io/bitfountain/matthewparker/taskit/Task.java
+++ app/src/main/java/io/bitfountain/matthewparker/taskit/Task.java
@@ -9,4 +9,28 @@ public class Task {
     private String mName;
     private Date mDueDate;
     private boolean mDone;
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+
+    public Date getDueDate() {
+        return mDueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        mDueDate = dueDate;
+    }
+
+    public boolean isDone() {
+        return mDone;
+    }
+
+    public void setDone(boolean done) {
+        mDone = done;
+    }
 }
